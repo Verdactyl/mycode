743266bff72ef6b7a4bbf2f8b05e29c5eaf8356e
Verdactyl <xijoeverix@gmail.com>
2021-04-12 16:58:47 +0000

    learning about lists and looping

---
+++ list.py
@@ -0,0 +1,13 @@
+
+#Joe= ["cheese", "wine"]
+#print(Joe[1])
+
+#Cameron= ["doritos", "coca cola"]
+#print(Cameron[1])
+
+#Rahim= ["margaritas", "salad"]
+#print(Rahim[0])
+
+megaparty= [4, "cases of soda", 39.4, "How much cheese can I eat?"]
+print(f"We need {megaparty[0]} {megaparty[1]}. {megaparty[-1]}  {megaparty[2]} slices. It was awesome.")
+
